c4d0842c7703fca19398d554c0591edd8d33cd0e
MiniBobbo <davidmurtagh@hotmail.com>
2019-08-23 15:55:11 -0400

    Removed assets

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -1,11 +1,12 @@
 export class Preload extends Phaser.Scene {
     preload() {
-        this.load.setBaseURL('http://labs.phaser.io');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
 
 
     }
     create() {
-        this.add.image(400,250, 'logo');
+        this.add.text(100,250, 'Community Game Jam')
+        .setFontSize(60)
+        .setAlign('center')
+        .setWordWrapWidth(600);
     }
 }
